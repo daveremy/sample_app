5a917f327014ca42d3ee36f4aa57c0bced785f1f
Dave Remy <email@daveremy.com>
2012-03-12 07:50:20 -0700

    Adding title in progress ... transfer to other machine

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "Home page" do
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', 'Sample App')
+    end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_celector('title',
+                                :text => "| Home")
+    end
+  end
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', 'Help')
+    end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_celector('title',
+                                :text => "| Help")
+    end
+  end
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', 'About Us')
+    end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_celector('title',
+                                :text => "| About")
+    end
+  end
+end
