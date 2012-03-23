8a5ebdd76ef8c1fa2230b330bc58bd0f6daca9d6
Dave Remy <email@daveremy.com>
2012-03-23 15:02:03 -0700

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,10 +15,16 @@ class UsersController < ApplicationController
                      password: user[:password],
                      password_confirmation: user[:password_confirmation])
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user 
     else
       render 'new'
     end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_path
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,7 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:remember_token) }
 
   it { should be_valid }
 
@@ -91,5 +92,10 @@ describe User do
   describe "with a password that's to short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
